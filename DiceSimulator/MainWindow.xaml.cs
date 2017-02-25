6efa4a4ff39b27a2160c68d3f48dec9a23200457
Nathan Rathbun <naterathbun@gmail.com>
2017-02-25 13:01:19 -0500

    Added some basic IO methods, created character class

--- DiceSimulator/MainWindow.xaml.cs
+++ DiceSimulator/MainWindow.xaml.cs
@@ -12,15 +12,52 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
 
 namespace DiceSimulator
 {
     public partial class MainWindow : Window
     {
+        public string CharacterFilePath { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        private void LoadCharacterButtonClicked(object sender, RoutedEventArgs e)
+        {
+            OpenFileBrowserAndSetFilePath();
+            // ChangeLoadFileButtonColorToDefault();
+            // ChangeControlButtonsToGreen();
+        }
+
+        public void OpenFileBrowserAndSetFilePath()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                SetFilePathOfTextFile(openFileDialog);
+            }
+        }
+
+        public void SetFilePathOfTextFile(OpenFileDialog openFileDialog)
+        {
+            this.CharacterFilePath = openFileDialog.FileName;
+        }
+
+        public void SetCharacterStatsFromFile(string filePath)
+        {
+            int linesInTextFile = File.ReadLines(filePath).Count();
+
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                while (!sr.EndOfStream)
+                {
+                    // create instance of character class and populate fields
+                }
+            }
+        }
     }
 }
